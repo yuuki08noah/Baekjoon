0d644b907f227077ca0a05f8f7b648e71b1aab54
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-06-21 07:55:23 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -3,19 +3,16 @@
 int n[1002];
 int main(void) {
     int l, m, res;
-    scanf("%d %d", &l, &m); // 7 3
-    for(int i = 2; i<=1000; i++) {
-        n[i]=i;
-    }
+    scanf("%d %d", &l, &m);
     int cnt = 0;
     for(int i = 2; i<=l; i++) {
-        if(n[i]==i) {
+        if(n[i]==0) {
             if(cnt == m) {
                 break;
             }
             for(int j = 1; j*i<=l; j++) {
-                if(n[j*i]!=0) {
-                    n[j*i] = 0; // 2*1 2*2 2*3
+                if(n[j*i]==0) {
+                    n[j*i] = 1; // 2*1 2*2 2*3
                     cnt++; // 1 2 3
                 }
                 if(cnt == m) { // X X O
