e9308b29a20abba39691bcbbf67cbe38ac42b211
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-08 07:59:43 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    while(n--) {
+        printf("%d\n", n+1);
+    }
+}
