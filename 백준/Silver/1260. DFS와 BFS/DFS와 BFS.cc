a4fa48d7ba758a08c04b5be17690757c569e9d17
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-09 17:20:12 +0900

    [Silver II] Title: DFS와 BFS, Time: 8 ms, Memory: 5944 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+int n, m, v;
+int arr[1001][1001];
+int flag1[1001]; // 1 2
+int flag2[1001];
+
+void dfs(int v) {
+    for(int l = 1; l<=n; l++) {
+        if((arr[v][l]==1 || arr[l][v]) && flag1[l]!=1) { // 2
+            flag1[l]=1;
+            printf("%d ", l);
+            dfs(l); // 1 2
+        }
+        else if(l==n) {
+            return;
+        }
+    }
+}
+
+int cnt=0;
+void bfs(int v, int k) {
+    int array[n];
+    for(int l = 1; l<=n; l++) {
+        if((arr[v][l]==1 || arr[l][v]) && flag2[l]!=1) { // 2
+            array[cnt]=l;
+            cnt++;
+            flag2[l]=1;
+            printf("%d ", l);
+        }
+        if(l==n && k==0) {
+            int a=cnt;
+            int i = 0;
+            cnt=0;
+            while(a--) {
+                bfs(array[i], a);
+                i++;
+            }
+        }
+    }
+}
+
+int main(void) {
+    scanf("%d %d %d", &n, &m, &v);
+    for(int i = 0; i<m; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        arr[a][b]=1;
+    }
+    printf("%d ", v);
+    flag1[v]=1;
+    flag2[v]=1;
+    dfs(v);
+    printf("\n%d ", v);
+    bfs(v, 0);
+    return 0;
+}
