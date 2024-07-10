d879a6a001b022d7073dbd3ee92e52fdecc833bf
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-11 08:19:44 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+    double w, h;
+    cin >> w >> h;
+    printf("%.1f", w*h/2);
+}
