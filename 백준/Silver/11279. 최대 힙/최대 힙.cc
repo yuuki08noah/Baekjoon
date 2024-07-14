10539aad84a078cd0d041145856aa60429998526
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-15 08:05:55 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <map>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <stdio.h>
+
+//#define MAX=10000000;
+
+using namespace std;
+
+int main(void) {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    priority_queue<int> arr;
+    for(int i = 0; i<n; i++) {
+        int num;
+        cin >> num;
+        if(num==0) {
+            if(arr.empty()) {
+                cout << 0 << '\n';
+            }
+            else {
+                cout << arr.top() << '\n';
+                arr.pop();
+            }
+        } else {
+            arr.push(num);
+        }
+    }
+}
