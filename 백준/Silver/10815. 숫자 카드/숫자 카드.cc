188a719fb9a8f2f96d7e26112f912200ecf75ac0
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-15 22:04:49 +0900

    [Silver V] Title: 숫자 카드, Time: 428 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <map>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <stdio.h>
+
+//#define MAX=10000000;
+
+using namespace std;
+
+int main(void) {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    map<int, int> map;
+    int n;
+    cin >> n;
+    while(n--) {
+        int num;
+        cin >> num;
+        map.insert({num, 1});
+    }
+    cin >> n;
+    while(n--) {
+        int num;
+        cin >> num;
+        cout << map.contains(num) << ' ';
+    }
+}
