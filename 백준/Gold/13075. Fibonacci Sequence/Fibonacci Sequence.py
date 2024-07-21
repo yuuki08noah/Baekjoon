3528e17fcbb897adda780bf44a994d9caf602a58
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-21 23:18:10 +0900

    [Gold II] Title: Fibonacci Sequence, Time: 144 ms, Memory: 111268 KB -BaekjoonHub

---
+++ 백준/Gold/13075. Fibonacci Sequence/Fibonacci Sequence.py
@@ -0,0 +1,37 @@
+import math
+import sys
+
+def m_m(arr, tp):
+    res = []
+    for i in range(len(arr)):
+        temp = []
+        for j in range(len(arr)):
+            sum = 0
+            for k in range(len(arr[i])):
+                sum += tp[i][k] * arr[k][j]
+            temp.append(sum%1000000000)
+        res.append(temp)
+    return res
+
+def power(base, exponent):
+    if exponent <= 1:
+        return base
+    elif (exponent % 2) == 1 and exponent > 1:
+        temp = power(base, (exponent - 1) // 2)
+        return m_m(m_m(temp, temp), base)
+    else:
+        temp = power(base, exponent // 2)
+        return m_m(temp, temp)
+
+if __name__ == "__main__":
+    m = int(sys.stdin.readline())
+    for i in range(m):
+        arr = [[1, 1], [1, 0]]
+        n = int(sys.stdin.readline())
+        if n == 0:
+            print(0)
+            continue
+        if n == -1:
+            break
+        res = power(arr, n)
+        print(res[0][1]%1000000000)
