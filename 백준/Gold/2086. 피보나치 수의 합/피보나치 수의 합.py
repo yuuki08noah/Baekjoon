f2ebe374eb4d3a64f5d2e82cb96179bb851359cb
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-21 22:55:00 +0900

    [Gold I] Title: 피보나치 수의 합, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2086. 피보나치 수의 합/피보나치 수의 합.py
@@ -0,0 +1,31 @@
+import math
+import sys
+
+def m_m(arr, tp):
+    res = []
+    for i in range(len(arr)):
+        temp = []
+        for j in range(len(arr)):
+            sum = 0
+            for k in range(len(arr[i])):
+                sum += tp[i][k] * arr[k][j]
+            temp.append(sum%1000000000)
+        res.append(temp)
+    return res
+
+def power(base, exponent):
+    if exponent <= 1:
+        return base
+    elif (exponent % 2) == 1 and exponent > 1:
+        temp = power(base, (exponent - 1) // 2)
+        return m_m(m_m(temp, temp), base)
+    else:
+        temp = power(base, exponent // 2)
+        return m_m(temp, temp)
+
+if __name__ == "__main__":
+    arr = [[1, 1], [1, 0]]
+    n, m = map(int, (sys.stdin.readline().split()))
+    res1 = power(arr, m + 2)
+    res2 = power(arr, n + 1)
+    print((res1[0][1] - res2[0][1])%1000000000)
