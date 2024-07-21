c142213d8d9e320ffdc9aa310057d3f1d2168ce1
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-21 23:02:51 +0900

    [Platinum V] Title: 피보나치 수의 제곱의 합, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Platinum/11440. 피보나치 수의 제곱의 합/피보나치 수의 제곱의 합.py
@@ -0,0 +1,31 @@
+import math
+import sys
+
+def m_m(arr, tp):
+    res = []
+    for i in range(len(arr)):
+        temp = []
+        for j in range(len(arr)):
+            sum = 0
+            for k in range(len(arr[i])):
+                sum += tp[i][k] * arr[k][j]
+            temp.append(sum%1000000007)
+        res.append(temp)
+    return res
+
+def power(base, exponent):
+    if exponent <= 1:
+        return base
+    elif (exponent % 2) == 1 and exponent > 1:
+        temp = power(base, (exponent - 1) // 2)
+        return m_m(m_m(temp, temp), base)
+    else:
+        temp = power(base, exponent // 2)
+        return m_m(temp, temp)
+
+if __name__ == "__main__":
+    arr = [[1, 1], [1, 0]]
+    n = int(sys.stdin.readline())
+    res1 = power(arr, n)
+    res2 = power(arr, n+1)
+    print((res1[0][1]*res2[0][1])%1000000007)
