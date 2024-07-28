9c036b300482cc77ed7240a914dcbf11fee5909e
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-07-28 20:08:46 +0900

    [Silver V] Title: 거스름돈, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,16 @@
+import sys
+
+
+input = sys.stdin.readline
+n = int(input())
+cnt = 0
+
+while n % 5 != 0:
+    n -= 2
+    cnt += 1
+    if n < 0:
+        print(-1)
+        exit()
+
+cnt += n//5
+print(cnt)
