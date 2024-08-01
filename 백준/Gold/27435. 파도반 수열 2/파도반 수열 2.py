279f02bf3c4af31ccad63b90dcdf0afb7b735103
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-08-01 14:38:59 +0900

    [Gold I] Title: 파도반 수열 2, Time: 276 ms, Memory: 114964 KB -BaekjoonHub

---
+++ 백준/Gold/27435. 파도반 수열 2/파도반 수열 2.py
@@ -0,0 +1,32 @@
+import math
+import sys
+
+def m_m(arr, tp):
+    res = []
+    for i in range(len(arr)):
+        temp = []
+        for j in range(len(arr)):
+            sum = 0
+            for k in range(len(arr[i])):
+                sum += tp[i][k] * arr[k][j]
+            temp.append(sum%998244353)
+        res.append(temp)
+    return res
+
+def power(base, exponent):
+    if exponent <= 1:
+        return base
+    elif (exponent % 2) == 1 and exponent > 1:
+        temp = power(base, (exponent - 1) // 2)
+        return m_m(m_m(temp, temp), base)
+    else:
+        temp = power(base, exponent // 2)
+        return m_m(temp, temp)
+
+if __name__ == "__main__":
+    t = int(sys.stdin.readline())
+    for _ in range(t):
+        arr = [[0, 1, 0], [0, 0, 1], [1, 1, 0]]
+        m = int(sys.stdin.readline())
+        res = power(arr, m)
+        print(res[2][1]%998244353)
