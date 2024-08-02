ec60d340ec3a14e287af1b3953fb72ff8b9cae10
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-08-02 17:39:00 +0900

    [Silver III] Title: 악수, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/8394. 악수/악수.py
@@ -0,0 +1,30 @@
+import math
+import sys
+
+def m_m(arr, tp):
+    res = []
+    for i in range(len(arr)):
+        temp = []
+        for j in range(len(arr)):
+            sum = 0
+            for k in range(len(arr[i])):
+                sum += tp[i][k] * arr[k][j]
+            temp.append(sum%10)
+        res.append(temp)
+    return res
+
+def power(base, exponent):
+    if exponent <= 1:
+        return base
+    elif (exponent % 2) == 1 and exponent > 1:
+        temp = power(base, (exponent - 1) // 2)
+        return m_m(m_m(temp, temp), base)
+    else:
+        temp = power(base, exponent // 2)
+        return m_m(temp, temp)
+
+if __name__ == "__main__":
+    arr = [[1, 1], [1, 0]]
+    m = int(sys.stdin.readline())
+    res = power(arr, m+1)
+    print(res[0][1]%10)
