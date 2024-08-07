77a8756496b5b80aaa1f051cf84c25e4845e64ea
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-08-08 03:10:15 +0900

    [Platinum V] Title: 선형 연립 방정식, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
+++ 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
@@ -13,16 +13,13 @@ for t in range(n-1): # 가우스 소거법
         for j in range(n+1):
             equations[i+1][j] -= mul * equations[t][j]
 
-res = []
-res.append(equations[-1][-1]/equations[-1][-2])
+res = [equations[-1][-1]/equations[-1][-2]]
 idx = 0
 for t in range(n-1, 0, -1):
     for i in range(0, t):
         equations[i][-1] -= equations[i][t]*res[-1]
-    if equations[t-1][t-1] == 0 and equations[t-1][-1] == 0:
-        res.append(1)
-    else:
-        res.append(equations[t-1][-1]/equations[t-1][t-1])
+    res.append(equations[t-1][-1]/equations[t-1][t-1])
     idx += 1
+
 for i in range(len(res)-1, -1, -1):
     print(int(round(res[i])), end=' ')
