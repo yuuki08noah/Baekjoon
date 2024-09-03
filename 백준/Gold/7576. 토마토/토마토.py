989ab01096a5143fd9dbb9286385c3ed0922449a
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-09-03 18:04:31 +0900

    [Gold V] Title: 토마토, Time: 388 ms, Memory: 156544 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,39 @@
+import sys
+import math
+from math import gcd
+from collections import deque
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+board = []
+for _ in range(m):
+    board.append(list(map(int, input().split())))
+def bfs(que):
+    queue = que
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]  # up, down, left, right
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n and board[nx][ny] != 2:
+                if board[nx][ny] == 0:
+                    board[nx][ny] += board[x][y] + 1
+                    queue.append((nx, ny))
+
+que = deque()
+for i in range(m):
+    for j in range(n):
+        if board[i][j] == 1:
+            que.append((i, j))
+bfs(que)
+
+for row in board:
+    if 0 in row:
+        print(-1)
+        exit()
+m_ = -10**9
+for row in board:
+    if m_ < max(row):
+        m_ = max(row)
+print(m_-1)
