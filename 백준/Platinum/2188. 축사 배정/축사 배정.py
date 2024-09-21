55278ae741596ece9cf19dbdcf713048c24dcf6d
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-09-22 06:19:22 +0900

    [Platinum IV] Title: 축사 배정, Time: 112 ms, Memory: 118092 KB -BaekjoonHub

---
+++ 백준/Platinum/2188. 축사 배정/축사 배정.py
@@ -0,0 +1,29 @@
+
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+table = {i:[] for i in range(1, n+1)}
+matched = {}
+
+for k in range(1, n+1):
+    arr = list(map(int, input().split()))
+    for i in range(1, len(arr)):
+        table[k].append(arr[i])
+
+def dfs(x):
+    for i in table[x]:
+        if visited[i-1]:
+            continue
+        visited[i-1] = True
+        if i not in matched or dfs(matched[i]):
+            matched[i] = x
+            return True
+    return False
+
+cnt = 0
+for i in range(1, n+1):
+    visited = [False] * m
+    if dfs(i):
+        cnt += 1
+print(cnt)
