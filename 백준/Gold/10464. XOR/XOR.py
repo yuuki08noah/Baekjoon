ca43b21504128457e0622f0a7eb7d1943f5f33a8
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-09-22 02:48:02 +0900

    [Gold IV] Title: XOR, Time: 228 ms, Memory: 128372 KB -BaekjoonHub

---
+++ 백준/Gold/10464. XOR/XOR.py
@@ -0,0 +1,19 @@
+import sys
+import decimal
+import cmath
+
+input = sys.stdin.readline
+n = int(input())
+def xor(x):
+    if x % 4 == 0:
+        return x
+    if x % 4 == 1:
+        return 1
+    if x % 4 == 2:
+        return x + 1
+    if x % 4 == 3:
+        return 0
+
+for i in range(n):
+    s, f = map(int, input().split())
+    print(xor(f) ^ xor(s-1))
