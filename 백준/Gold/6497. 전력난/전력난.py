8a36ee388e9b015de85043300c4667c4fd3e1f83
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-09-28 23:49:02 +0900

    [Gold IV] Title: 전력난, Time: 2704 ms, Memory: 301944 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+import heapq
+
+input = sys.stdin.readline
+
+
+def find(x):
+    if parents[x] == -1:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
+
+def union(x, y):
+    px, py = find(x), find(y)
+    if px!= py:
+        parents[px] = py
+while True:
+    cmd = []
+    n, m = map(int, input().split())
+    parents = [-1 for i in range(n)]
+    if n == m == 0:
+        exit()
+    sys.setrecursionlimit(4*n)
+    s = 0
+    for i in range(m):
+        temp = list(map(int, input().split()))
+        s += temp[2]
+        heapq.heappush(cmd, (temp[2], temp[0], temp[1]))
+
+    res = 0
+    for i in range(m):
+        cost, a, b = heapq.heappop(cmd)
+
+        if find(a-1) != find(b-1):
+            union(a-1, b-1)
+            res += cost
+
+    print(s - res)
