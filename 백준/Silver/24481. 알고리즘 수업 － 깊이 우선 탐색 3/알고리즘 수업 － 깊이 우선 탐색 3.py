6cd4c92d04a4dbe277d510e50620007175c6d155
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-10-18 10:14:59 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 828 ms, Memory: 86672 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+n, m, k = map(int, input().split())
+table = {i:[] for i in range(1, n + 1)}
+for i in range(m):
+    a, b = map(int, input().split())
+    table[a].append(b)
+    table[b].append(a)
+
+depths = [-1 for _ in range(n)]
+
+def dfs(node, depth):
+    if depths[node-1] == -1:
+        depths[node-1] = depth
+        for i in sorted(table[node]):
+            dfs(i, depth + 1)
+
+dfs(k, 0)
+
+print(*depths, sep='\n')
