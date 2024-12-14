8b336e130076a87bbf2e955f123052ed3d131bfd
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-12-14 15:08:03 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 120 ms, Memory: 112084 KB -BaekjoonHub

---
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+queue = deque([(0, 0)])
+while queue:
+    x, y = queue.popleft()
+    if visited[x][y]: continue
+    visited[x][y] = True
+    if n > y + board[x][y] >= 0 and not visited[x][y + board[x][y]]:
+        queue.append((x, y + board[x][y]))
+    if n > x + board[x][y] >= 0 and not visited[x + board[x][y]][y]:
+        queue.append((x + board[x][y], y))
+print("HaruHaru" if visited[-1][-1] else "Hing")
