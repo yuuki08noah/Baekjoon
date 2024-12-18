2ccb96622a34dc51dfe7019595cf06042acb44fd
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-12-18 10:37:13 +0900

    [Silver IV] Title: Router, Time: 92 ms, Memory: 3352 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15828. Router/Router.go
+++ 백준/Silver/15828. Router/Router.go
@@ -47,30 +47,36 @@ func (q *CircularQueue[T]) Dequeue() (T, error) {
 		return value, errors.New("Queue is empty")
 	}
 	q.front = (q.front + 1) % q.size
-	q.data[q.front] = value
+	value = q.data[q.front]
 	return value, nil
 }
 
 func (q *CircularQueue[T]) PrintCircularQueue() {
-	for i := (q.front + 1) % q.size; (i % q.size) != q.rear+1; i++ {
-		fmt.Print(q.data[i], " ")
+	for i := (q.front + 1) % q.size; i != (q.rear+1)%q.size; i = (i + 1) % q.size {
+		fmt.Fprint(writer, q.data[i], " ")
 	}
-	fmt.Println()
+	fmt.Fprintln(writer)
 }
 
 func main() {
 	defer writer.Flush()
 	var n, m int
 	fmt.Fscanln(reader, &n)
-	q := InitCircularQueue[int](n)
+	q := InitCircularQueue[int](n + 1)
 	for {
 		fmt.Fscanln(reader, &m)
 		if m == -1 {
 			break
 		} else if m != 0 {
-			q.Enqueue(m)
+			err := q.Enqueue(m)
+			if err != nil {
+				continue
+			}
 		} else {
-			q.Dequeue()
+			_, err := q.Dequeue()
+			if err != nil {
+				continue
+			}
 		}
 	}
 	if q.isEmpty() {
