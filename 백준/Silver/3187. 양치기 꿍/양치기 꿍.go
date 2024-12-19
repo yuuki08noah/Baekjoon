29cb19e9d7d76aa59fb7244fd3b4a3f1e1b786b6
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2024-12-19 22:36:41 +0900

    [Silver I] Title: 양치기 꿍, Time: 20 ms, Memory: 6120 KB -BaekjoonHub

---
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var reader = bufio.NewReader(os.Stdin)
+	var writer = bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	var n, m int
+	var graph [][]rune
+	dx := []int{0, 1, 0, -1}
+	dy := []int{1, 0, -1, 0}
+	var shres, vres = 0, 0
+	fmt.Fscanln(reader, &n, &m)
+	for i := 0; i < n; i++ {
+		var temp []rune
+		for j := 0; j <= m; j++ {
+			var value rune
+			fmt.Fscanf(reader, "%c", &value)
+			if value == '\n' {
+				continue
+			}
+			temp = append(temp, value)
+		}
+		graph = append(graph, temp)
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if graph[i][j] == '.' || graph[i][j] == 'v' || graph[i][j] == 'k' {
+				var queue [][]int
+				queue = append(queue, []int{i, j})
+				var sheep, wolf = 0, 0
+				for len(queue) > 0 {
+					x, y := queue[0][0], queue[0][1]
+					queue = queue[1:]
+					if graph[x][y] == '^' {
+						continue
+					}
+					if graph[x][y] == 'k' {
+						sheep++
+					} else if graph[x][y] == 'v' {
+						wolf++
+					}
+					graph[x][y] = '^'
+					for k := 0; k < 4; k++ {
+						nx, ny := x+dx[k], y+dy[k]
+						if nx >= 0 && nx < n && ny >= 0 && ny < m {
+							if graph[nx][ny] == '.' || graph[nx][ny] == 'v' || graph[nx][ny] == 'k' {
+								queue = append(queue, []int{nx, ny})
+							}
+						}
+					}
+				}
+				if sheep > wolf {
+					shres += sheep
+				} else {
+					vres += wolf
+				}
+			}
+		}
+	}
+	fmt.Fprintln(writer, shres, vres)
+}
