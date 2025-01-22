33b0728f3bec5c6186e5b1147e0961d049fbff71
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2025-01-22 15:50:17 +0900

    [Silver IV] Title: 순서쌍의 곱의 합, Time: 144 ms, Memory: 124940 KB -BaekjoonHub

---
+++ 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.py
@@ -0,0 +1,15 @@
+import copy
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+l = copy.deepcopy(arr)
+s = 0
+for i in range(1, len(arr)):
+    l[i] += l[i-1]
+
+for i in range(0, len(arr)):
+    s += arr[i]*(l[-1]-l[i])
+
+print(s)
