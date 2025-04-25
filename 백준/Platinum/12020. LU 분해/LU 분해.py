cee864457cfc12bff0d1f5e90f923773d3d4bd2e
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2025-04-25 11:48:32 +0900

    [Platinum V] Title: LU 분해, Time: 532 ms, Memory: 132360 KB -BaekjoonHub

--- 백준/Platinum/12020. LU 분해/LU 분해.py
+++ 백준/Platinum/12020. LU 분해/LU 분해.py
@@ -6,27 +6,33 @@ equations = []
 for i in range(n):
     equations.append(list(map(int, input().split())))
 
-U = [row[:] for row in equations]
+U = []
 L = [[0.0] * n for _ in range(n)]
 for i in range(n):
     L[i][i] = 1.0
 
-for t in range(1, n):
-    if U[t-1][t-1] == 0:
-        print(-1)
-        exit()
-    for i in range(t, n):
-        mul = U[i][t-1] / U[t-1][t-1]
-        L[i][t-1] = mul
-        for j in range(t-1, n):
-            U[i][j] -= mul * U[t-1][j]
-
-for i in range(n):
-    if U[i][i] == 0:
-        print(-1)
-        exit()
-
-for row in L:
-    print(' '.join(f"{elem:.3f}" for elem in row))
-for row in U:
-    print(' '.join(f"{elem:.3f}" for elem in row))
+U.append(equations[0])
+try:
+    for t in range(1, n):
+        mul = equations[t][t-1] / equations[t-1][t-1]
+        u_temp = []
+        for j in range(n):
+            u_temp.append(equations[t][j] - mul * equations[t-1][j])
+            equations[t][j] -= mul * equations[t-1][j]
+        for j in range(t):
+            L[t][j] = mul
+        U.append(u_temp)
+    for i in range(n):
+        if U[i][i] == 0:
+            print(-1)
+            exit()
+    for i in range(n):
+        for j in range(n):
+            print(f"{L[i][j]:.3f}", end=' ')
+        print()
+    for i in range(n):
+        for j in range(n):
+            print(f"{U[i][j]:.3f}", end=' ')
+        print()
+except ZeroDivisionError:
+    print("-1")
