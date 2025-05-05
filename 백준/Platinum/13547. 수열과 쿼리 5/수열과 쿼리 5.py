63b090630affdb842440f615e561a1aad1b7d9d6
방세준 <163106279+yuuki08noah@users.noreply.github.com>
2025-05-05 23:12:58 +0900

    [Platinum II] Title: 수열과 쿼리 5, Time: 732 ms, Memory: 148092 KB -BaekjoonHub

--- 백준/Platinum/13547. 수열과 쿼리 5/수열과 쿼리 5.py
+++ 백준/Platinum/13547. 수열과 쿼리 5/수열과 쿼리 5.py
@@ -6,47 +6,28 @@ n = int(input())
 arr = [0] + list(map(int, input().split()))
 q = int(input())
 b = int(math.sqrt(n))
-
-# 쿼리에 인덱스 추가
-queries = [(tuple(map(int, input().split())), i) for i in range(q)]
-queries.sort(key=lambda x: (x[0][0]//b, x[0][1]))  # right를 직접 비교
-
-# appeared 배열 크기를 arr의 최대 값에 맞춤
-max_val = max(arr)
-appeared = [0] * (max_val + 1)
+queries = sorted([(tuple(map(int, input().split())), i) for i in range(q)], key=lambda x: (x[0][0]//b, x[0][1]))
+appeared = [0] * (max(arr)+1)
+start, end = queries[0][0][0], queries[0][0][0]-1
 cnt = 0
 results = [0] * q
-
-def add(x):
-    global cnt
-    appeared[arr[x]] += 1
-    if appeared[arr[x]] == 1:
-        cnt += 1
-
-def remove(x):
-    global cnt
-    appeared[arr[x]] -= 1
-    if appeared[arr[x]] == 0:
-        cnt -= 1
-
-# 초기 구간 설정
-start, end = queries[0][0][0], queries[0][0][0] - 1
-
-for (left, right), idx in queries:
+for query, idx in queries:
+    left, right = query
     while start < left:
-        remove(start)
+        appeared[arr[start]] -= 1
+        if appeared[arr[start]] == 0: cnt -= 1
         start += 1
     while start > left:
         start -= 1
-        add(start)
+        appeared[arr[start]] += 1
+        if appeared[arr[start]] == 1: cnt += 1
     while end < right:
         end += 1
-        add(end)
+        appeared[arr[end]] += 1
+        if appeared[arr[end]] == 1: cnt += 1
     while end > right:
-        remove(end)
+        appeared[arr[end]] -= 1
+        if appeared[arr[end]] == 0: cnt -= 1
         end -= 1
     results[idx] = cnt
-
-# 입력 순서대로 출력
-for res in results:
-    print(res)
+print(*results, sep='\n')
